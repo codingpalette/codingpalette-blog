899c432ee0cce465f00e3eb7efa9fe50130ba984
codingpalette <codingpalette@gmail.com>
2021-11-15 20:43:57 +0900

    로그인 페이지 수정

--- src/routes/LoginPage/index.js
+++ src/routes/LoginPage/index.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Header from '../../components/Header'
 import { LoginContainer } from './styles'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
+import useInput from '../../hooks/useInput'
 
 const LoginPage = () => {
+  const [id, onChangeId] = useInput('')
+  const [password, onChangePassword] = useInput('')
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault()
+      console.log(id)
+      console.log(password)
+      console.log('1231')
+    },
+    [id, password],
+  )
+
   return (
     <>
       <Header />
       <LoginContainer>
         <div className="card_box">
           <h3>LOGIN</h3>
-          <div className="form_group">
+          <form className="form_group" onSubmit={onSubmit}>
             <div className="input_group">
               <label htmlFor="id">아이디</label>
-              <Input id="id" />
+              <Input value={id} onChange={onChangeId} id="id" required />
             </div>
             <div className="input_group">
               <label htmlFor="id">비밀번호</label>
-              <Input id="password" type="password" />
+              <Input value={password} onChange={onChangePassword} id="password" type="password" required />
             </div>
             <div className="btn_box">
-              <Button width="100%">로그인</Button>
+              <Button width="100%" type="submit">
+                로그인
+              </Button>
               <Button width="100%">회원가입 하러 가기</Button>
             </div>
-          </div>
+          </form>
         </div>
       </LoginContainer>
     </>
