39ccd6f26fdc4c4b9020a04fcef9e912f3560441
codingpalette <codingpalette@gmail.com>
2021-11-15 20:42:29 +0900

    hooks 수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react'
 
-const useInput = initialValue => {
+const useInput = (initialValue = null) => {
   const [value, setValue] = useState(initialValue)
   const handler = useCallback(e => {
     setValue(e.target.value)
