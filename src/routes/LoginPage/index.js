a36b181c764325c98b8140aaf581cb5375fb53c3
codingpalette <codingpalette@gmail.com>
2021-11-16 19:27:20 +0900

    login 페이지 수정

--- src/routes/LoginPage/index.js
+++ src/routes/LoginPage/index.js
@@ -1,11 +1,15 @@
 import React, { useCallback } from 'react'
 import Header from '../../components/Header'
-import { LoginContainer } from './styles'
+import { FormGroup } from './styles'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import useInput from '../../hooks/useInput'
+import { useNavigate } from 'react-router-dom'
+import AuthContainer from '../../containers/AuthContainer'
+import { CardBox } from '../../components/Card/styles'
 
 const LoginPage = () => {
+  const navigate = useNavigate()
   const [id, onChangeId] = useInput('')
   const [password, onChangePassword] = useInput('')
   const onSubmit = useCallback(
@@ -21,27 +25,31 @@ const LoginPage = () => {
   return (
     <>
       <Header />
-      <LoginContainer>
-        <div className="card_box">
-          <h3>LOGIN</h3>
-          <form className="form_group" onSubmit={onSubmit}>
-            <div className="input_group">
-              <label htmlFor="id">아이디</label>
-              <Input value={id} onChange={onChangeId} id="id" required />
-            </div>
-            <div className="input_group">
-              <label htmlFor="id">비밀번호</label>
-              <Input value={password} onChange={onChangePassword} id="password" type="password" required />
-            </div>
-            <div className="btn_box">
-              <Button width="100%" type="submit">
-                로그인
-              </Button>
-              <Button width="100%">회원가입 하러 가기</Button>
-            </div>
-          </form>
-        </div>
-      </LoginContainer>
+      <AuthContainer>
+        <CardBox>
+          <FormGroup>
+            <h3>LOGIN</h3>
+            <form onSubmit={onSubmit}>
+              <div className="input_group">
+                <label htmlFor="id">아이디</label>
+                <Input value={id} onChange={onChangeId} id="id" required />
+              </div>
+              <div className="input_group">
+                <label htmlFor="password">비밀번호</label>
+                <Input value={password} onChange={onChangePassword} id="password" type="password" required />
+              </div>
+              <div className="btn_box">
+                <Button width="100%" type="submit">
+                  로그인
+                </Button>
+                <Button width="100%" onClick={() => navigate('/join')}>
+                  회원가입 하러 가기
+                </Button>
+              </div>
+            </form>
+          </FormGroup>
+        </CardBox>
+      </AuthContainer>
     </>
   )
 }
