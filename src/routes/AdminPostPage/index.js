68c82dd33282bd05bfd21776a3216759eb01f5d3
codingpalette <codingpalette@gmail.com>
2021-11-28 21:04:30 +0900

    어드민 포스터 페이지 생성

---
+++ src/routes/AdminPostPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminHeader from '../../components/AdminHeader'
+import AdminContainer from '../../containers/AdminContainer'
+
+const AdminPostPage = () => {
+  return (
+    <>
+      <AdminHeader title="Post" />
+      <AdminContainer>sdfsdsdf</AdminContainer>
+    </>
+  )
+}
+
+export default AdminPostPage
---
+++ src/routes/AdminPostPage/styles.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  padding: 0 1rem;
+  box-sizing: border-box;
+`
