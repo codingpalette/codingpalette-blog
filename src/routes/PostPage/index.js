21548ae9d40de9e69a35a01ee736564760e44dd5
codingpalette <codingpalette@gmail.com>
2021-12-13 21:10:24 +0900

    post page 수정

--- src/routes/PostPage/index.js
+++ src/routes/PostPage/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
+import MainContainer from '../../containers/MainContainer'
+import { useParams } from 'react-router-dom'
+import { getPost } from '../../models/post'
+import { Viewer } from '@toast-ui/react-editor'
+import { PostContainer } from './styles'
 
 const PostPage = () => {
+  let params = useParams()
+  const [content, setContent] = useState('')
+  const [pageLoading, setPageLoading] = useState(false)
+
+  useEffect(() => {
+    getPostEvent()
+  }, [])
+
+  const getPostEvent = async () => {
+    try {
+      const res = await getPost(params.id)
+      console.log(res)
+      setContent(res.content)
+      setPageLoading(true)
+    } catch (e) {
+      console.error(e)
+    }
+  }
   return (
     <>
       <Header />
-      <div>sf</div>
+      <MainContainer>
+        {pageLoading && (
+          <>
+            <PostContainer>
+              <div>sf</div>
+              <Viewer initialValue={content} />
+            </PostContainer>
+          </>
+        )}
+      </MainContainer>
     </>
   )
 }
---
+++ src/routes/PostPage/styles.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const PostContainer = styled.div`
+  width: 768px;
+  padding-top: 5.5rem;
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
+`
