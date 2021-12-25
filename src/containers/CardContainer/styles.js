2684b3fbb7594ba16b9dcce0af8b49172d7b591d
codingpalette <codingpalette@gmail.com>
2021-12-25 10:21:50 +0900

    cardcontainer 수정

--- src/containers/CardContainer/styles.js
+++ src/containers/CardContainer/styles.js
@@ -3,7 +3,19 @@ import styled from '@emotion/styled'
 export const Container = styled.div`
   padding-top: 1rem;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 1rem;
   box-sizing: border-box;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
